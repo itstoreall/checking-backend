d3505d7013228b4dbdd06b96db7419139fe923fa
Serhii Stanislav <serhiistanislav@gmail.com>
2022-11-16 13:49:48 +0200

    ADD | some changes

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import routes from './routes';
 const app = express();
 
 const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 3000;
 
 app.use('*', cors());
 app.use(express.json());
